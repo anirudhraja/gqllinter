286c3e5339a9c00cdffa3829e93533ec47531608
Sonakshi <sonakshi.gupta@uber.com>
2025-11-07 15:04:15 +0530

    Update types-have-descriptions to skip root operation types

--- pkg/rules/rules_test.go
+++ pkg/rules/rules_test.go
@@ -82,6 +82,78 @@ func TestTypesHaveDescriptions(t *testing.T) {
 			t.Error("Expected no errors for type with description")
 		}
 	})
+
+	t.Run("should pass builtin types without descriptions", func(t *testing.T) {
+		schema := `
+		type Query {
+			id: ID!
+			name: String
+			active: Boolean
+			age: Int
+			score: Float
+		}
+		`
+		errors := runRule(t, rule, schema)
+		if countRuleErrors(errors, "types-have-descriptions") > 0 {
+			t.Error("Expected no errors for type with description")
+		}
+	})
+
+	t.Run("should pass default root operation types without descriptions", func(t *testing.T) {
+		schema := `
+		type Query {
+			hello: String
+		}
+
+		type Mutation {
+			doThing: String
+		}
+
+		type Subscription {
+			ping: String
+		}
+
+		"""A user in the system"""
+		type User {
+			id: ID!
+		}
+		`
+		errors := runRule(t, rule, schema)
+		if countRuleErrors(errors, "types-have-descriptions") > 0 {
+			t.Error("Expected no errors for type with description")
+		}
+	})
+
+	t.Run("should pass custom root operation types without descriptions", func(t *testing.T) {
+		schema := `
+		schema {
+			query: RootQuery
+			mutation: RootMutation
+			subscription: RootSubscription
+		}
+
+		type RootQuery {
+			hello: String
+		}
+
+		type RootMutation {
+			doThing: String
+		}
+
+		type RootSubscription {
+			ping: String
+		}
+
+		"""A user in the system"""
+		type User {
+			id: ID!
+		}
+		`
+		errors := runRule(t, rule, schema)
+		if countRuleErrors(errors, "types-have-descriptions") > 0 {
+			t.Error("Expected no errors for type with description")
+		}
+	})
 }
 
 func TestFieldsHaveDescriptions(t *testing.T) {
